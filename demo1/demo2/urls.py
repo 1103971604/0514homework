385c32bd7e68301186bb45937b8829766d69672c
赵宇杰 <1103971604@qq.com>
2019-05-16 19:46:11 +0800

    2.16课上练习。

--- demo1/demo2/urls.py
+++ demo1/demo2/urls.py
@@ -4,9 +4,13 @@ from . import views
 app_name='demo2url'
 
 urlpatterns = [
-    url(r'^dem123123o2list/$', views.list,name='list'),
-    url(r'^dem213123o2hero/(\d+)/$', views.hero,name='hero'),
-    url(r'^index/$',views.index,name='index')
+    url(r'^list/$', views.list,name='list'),
+    url(r'^hero/(\d+)/$', views.hero,name='hero'),
+    url(r'^index/$',views.index,name='index'),
+    url(r'^deletebook/(\d+)/$',views.deletebook,name='deletebook'),
+    url(r'^deletehero/(\d+)$',views.deletehero,name='deletehero'),
+    url(r'^addhero/(\d+)/$',views.addhero,name='addhero'),
+    url(r'^addbook/$',views.addbook,name='addbook'),
 ]
 
 
--- demo1/demo2/views.py
+++ demo1/demo2/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from .models import Bookinfo,Heroinfo
 # Create your views here.
@@ -14,6 +14,8 @@ def index(request):
     return HttpResponse(result)
 
 def list(request):
+
+    # if request.method=='GET':
     # 获取模板--参数制定了模板位置为demo2文件夹下面的index.html
     temp = loader.get_template('demo2/list.html')
     # 渲染数据--参数是要发送给模板的数据此处用了{}表示不传数据
@@ -21,6 +23,12 @@ def list(request):
     result = temp.render({'books':books})   #将数据渲染模板
     # 返回
     return HttpResponse(result)
+    # elif request.method=='POST':
+    #     book=request.POST['bookname']
+    #     b1=Bookinfo()
+    #     b1.title=book
+    #     b1.save()
+    #     return HttpResponseRedirect('/demo2/list/')
 
 def hero(request,bookid):
     # 获取模板--参数制定了模板位置为demo2文件夹下面的index.html
@@ -30,3 +38,41 @@ def hero(request,bookid):
     result = temp.render({'book':book})
     # 返回
     return HttpResponse(result)
+
+def deletebook(request,id):
+    Bookinfo.objects.get(pk=id).delete()
+    # return HttpResponse('成功')
+    return HttpResponseRedirect('/demo2/list/')
+
+def addbook(request):
+    if request.method == 'GET':
+        return render(request,'demo2/addbook.html')
+    elif request.method=='POST':
+        book=request.POST['bookname']
+        b1=Bookinfo()
+        b1.title=book
+        b1.save()
+        return HttpResponseRedirect('/demo2/list/')
+
+def deletehero(request,id):
+    # return HttpResponse('成功')
+    hero=Heroinfo.objects.get(pk=id)
+
+    a=hero.book.id
+    hero.delete()
+    return HttpResponseRedirect('/demo2/hero/%s/'%(a,))
+
+
+def addhero(request,bookid):
+    if request.method=='GET':
+        return render(request,'demo2/addhero.html',{'bookid':bookid})
+    elif request.method=='POST':
+        obj=Bookinfo.objects.get(pk=bookid)
+        h1=Heroinfo()
+        h1.name=request.POST['username']
+        h1.age=request.POST['age']
+        h1.skill=request.POST['skill']
+        h1.book=obj
+        h1.save()
+        return HttpResponseRedirect('/demo2/hero/%s/'%(bookid,))
+
